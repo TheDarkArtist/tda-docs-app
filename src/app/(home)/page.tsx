e020bf1a3c0efaa44281e5a4157ebf709959aeda
Kushagra <sparrow.kushagra@gmail.com>
2025-01-04 03:11:53 +0530

    chore(app): move 'page' to 'app/(home)/page' for creating page-specific navbar

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,8 +4,12 @@ const Home = () => {
   return (
     <div className="h-screen flex flex-col gap-y-2 justify-center items-center">
       <p className="text-3xl">hello world</p>
-      <Link className="text-blue-600" href="/docs/123">go to /docs/123</Link>
-
+      <Link
+        className="text-blue-600"
+        href="/docs/123"
+      >
+        go to /docs/123
+      </Link>
     </div>
   );
 };
