41e073685abccb09832fc546b36e590c8fe7f11a
Kushagra <sparrow.kushagra@gmail.com>
2025-01-05 07:04:10 +0530

    feat(liveblocks): Add Mention suggestions and avatars of users in the room

--- src/app/docs/[documentId]/page.tsx
+++ src/app/docs/[documentId]/page.tsx
@@ -9,17 +9,17 @@ interface DocumentPageProps {
 
 const DocumentPage = async ({}: DocumentPageProps) => {
   return (
-    <div className="min-h-screen bg-[#FAFBFD]">
-      <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-neutral-50 print:hidden">
-        <Navbar />
-        <Toolbar />
-      </div>
-      <div className="pt-28 print:pt-0">
-        <Room>
+    <Room>
+      <div className="min-h-screen bg-[#FAFBFD]">
+        <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-neutral-50 print:hidden">
+          <Navbar />
+          <Toolbar />
+        </div>
+        <div className="pt-28 print:pt-0">
           <Editor />
-        </Room>
+        </div>
       </div>
-    </div>
+    </Room>
   );
 };
 
--- src/app/docs/[documentId]/room.tsx
+++ src/app/docs/[documentId]/room.tsx
@@ -1,23 +1,69 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useMemo, useState } from "react";
 import {
   LiveblocksProvider,
   RoomProvider,
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import { useParams } from "next/navigation";
+import { FullScreenLoader } from "@/components/full-screen-loader";
+import { getUsers } from "./actions";
+import { useToast } from "@/hooks/use-toast";
+
+type User = { id: string; name: string; avatar: string };
 
 export function Room({ children }: { children: ReactNode }) {
   const params = useParams();
+  const { toast } = useToast();
+
+  const [users, setUsers] = useState<User[]>([]);
+
+  const fetchUsers = useMemo(
+    () => async () => {
+      try {
+        const list = await getUsers();
+        setUsers(list);
+      } catch {
+        toast({
+          title: "Error",
+          description: "Something went wrong",
+          variant: "destructive",
+        });
+      }
+    },
+    [toast],
+  );
+
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
 
   return (
     <LiveblocksProvider
       throttle={16}
       authEndpoint="/api/liveblocks-auth"
+      resolveUsers={({ userIds }) => {
+        return userIds.map(
+          (userId) => users.find((user) => user.id === userId) ?? undefined,
+        );
+      }}
+      resolveMentionSuggestions={({ text }) => {
+        let filteredUsers = users;
+
+        if (text) {
+          filteredUsers = users.filter((user) =>
+            user.name.toLowerCase().includes(text.toLowerCase()),
+          );
+        }
+        return filteredUsers.map((user) => user.id);
+      }}
+      resolveRoomsInfo={() => []}
     >
       <RoomProvider id={params.documentId as string}>
-        <ClientSideSuspense fallback={<div>Loading…</div>}>
+        <ClientSideSuspense
+          fallback={<FullScreenLoader label="Setting up the room for you" />}
+        >
           {children}
         </ClientSideSuspense>
       </RoomProvider>
